84488e126f8d92a50fbfab707f259426af514045
cmw9957 <cmw9957@gmail.com>
2024-01-04 17:34:03 +0900

    🎉[FEAT] : 스낵바 구현

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ setTimeout(function () {
         document.getElementById('main-content').style.display = 'block';
     }, 1000);
 }, 3000);
+
+window.addEventListener('load', function() {
+    document.getElementById('permissionSnackbar').style.display = 'block';
+});
+
+document.getElementById('closeSnackbarBtn').addEventListener('click', function() {
+    document.getElementById('permissionSnackbar').style.display = 'none';
+});
