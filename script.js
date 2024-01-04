8739bfc35f81d39432c57a80f23649f55e4a1c04
cmw9957 <cmw9957@gmail.com>
2024-01-04 20:48:38 +0900

    🎁[REFACTOR] : script.js 함수 리팩토링

--- script.js
+++ script.js
@@ -1,44 +1,59 @@
-setTimeout(function () {
-    document.getElementById('splash-container').classList.add('hide');
-
-    
+function splashPage() {
+    console.log("asdsadsadsad");
     setTimeout(function () {
-        document.getElementById('splash-container').style.display = 'none';
-        document.getElementById('main-content').style.display = 'block';
-    }, 1000);
-}, 3000);
+        document.getElementById('splash-container').classList.add('hide');
+    
+        
+        setTimeout(function () {
+            document.getElementById('splash-container').style.display = 'none';
+            document.getElementById('main-content').style.display = 'block';
+        }, 1000);
+    }, 1500);
+}
 
-window.addEventListener('load', function() {
-    document.getElementById('permissionSnackbar').style.display = 'block';
-});
+function snackBar() {
+    window.addEventListener('load', function() {
+        document.getElementById('permissionSnackbar').style.display = 'block';
+    });
+    
+    document.getElementById('closeSnackbarBtn').addEventListener('click', function() {
+        document.getElementById('permissionSnackbar').style.display = 'none';
+    });
+}
 
-document.getElementById('closeSnackbarBtn').addEventListener('click', function() {
-    document.getElementById('permissionSnackbar').style.display = 'none';
-});
+function getPosition() {
+    const currentGeoLocation = document.getElementById("closeSnackbarBtn");
 
-const currentGeoLocation = document.getElementById("closeSnackbarBtn");
+    currentGeoLocation.onclick = function() {
+        var startPos;
+        var geoOptions = {
+            timeout: 10 * 1000
+        };
 
-currentGeoLocation.onclick = function() {
-    var startPos;
-    var geoOptions = {
-        timeout: 10 * 1000
+        let geoSuccess = function (position) {
+            // Do magic with location
+            startPos = position;
+            const latitude = startPos.coords.latitude;
+            const longitude = startPos.coords.longitude;
+            console.log(latitude, longitude);
+        };
+        let geoError = function (error) {
+            console.log('Error occurred. Error code: ' + error.code);
+            // error.code can be:
+            //   0: unknown error
+            //   1: permission denied
+            //   2: position unavailable (error response from location provider)
+            //   3: timed out
+        };
+
+        navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
     };
+}
 
-    let geoSuccess = function (position) {
-        // Do magic with location
-        startPos = position;
-        const latitude = startPos.coords.latitude;
-        const longitude = startPos.coords.longitude;
-        console.log(latitude, longitude);
-    };
-    let geoError = function (error) {
-        console.log('Error occurred. Error code: ' + error.code);
-        // error.code can be:
-        //   0: unknown error
-        //   1: permission denied
-        //   2: position unavailable (error response from location provider)
-        //   3: timed out
-    };
+function main() {
+    splashPage();
+    snackBar();
+    getPosition();
+}
 
-    navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
-};
+main();
