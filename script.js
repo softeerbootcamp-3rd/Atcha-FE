acb773ab7722aa4c616817e012d9c78e9af73c9a
cmw9957 <cmw9957@gmail.com>
2024-01-04 18:10:40 +0900

    🎉[FEAT] : 카메라, 위치 호출 기능 구현

--- script.js
+++ script.js
@@ -15,3 +15,30 @@ window.addEventListener('load', function() {
 document.getElementById('closeSnackbarBtn').addEventListener('click', function() {
     document.getElementById('permissionSnackbar').style.display = 'none';
 });
+
+const currentGeoLocation = document.getElementById("closeSnackbarBtn");
+
+currentGeoLocation.onclick = function() {
+    var startPos;
+    var geoOptions = {
+        timeout: 10 * 1000
+    };
+
+    let geoSuccess = function (position) {
+        // Do magic with location
+        startPos = position;
+        const latitude = startPos.coords.latitude;
+        const longitude = startPos.coords.longitude;
+        console.log(latitude, longitude);
+    };
+    let geoError = function (error) {
+        console.log('Error occurred. Error code: ' + error.code);
+        // error.code can be:
+        //   0: unknown error
+        //   1: permission denied
+        //   2: position unavailable (error response from location provider)
+        //   3: timed out
+    };
+
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
+};
