3ada7a9a18c434de9848ea6da022f1b8eee3d3d7
cmw9957 <cmw9957@gmail.com>
2024-01-07 19:46:54 +0900

    📗[DOCS] : 간략한 설명 주석 작성

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// 메인 화면 하단의 주차 정보 출력 관련 기능 
 document.addEventListener('DOMContentLoaded', function () {
     const toggleButton = document.getElementById('toggleButton');
     const list = document.getElementById('list');
@@ -14,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-
+// 실행시 처음 스플래쉬 효과 관련 함수
 function splashPage() {
     setTimeout(function () {
         document.getElementById('splash-container').classList.add('hide');
@@ -31,12 +32,13 @@ function splashEffect(){
         // Check if the splash screen should be displayed
         const splashScreenDisplayed = localStorage.getItem('splashScreenDisplayed');
         console.log(splashScreenDisplayed);
+        // 맨 처음 접속 했을 때
         if (!splashScreenDisplayed) {
             // Show the splash screen
             splashPage();
             // Set a flag to indicate that the splash screen has been displayed
             localStorage.setItem('splashScreenDisplayed', 'true');
-        }else{
+        }else{ // 2번 이상 접속 했을 때 스플래쉬 효과가 나오지 않도록
             document.getElementById('splash-container').style.display = 'none';
             document.getElementById('main-content').style.display = 'block';
         }
@@ -44,12 +46,15 @@ function splashEffect(){
     });
 }
 
+// 사용자 알람, 위치 권한 요청을 위한 스낵바
 function snackBar() {
     window.addEventListener('load', function() {
         const notificationGranted = localStorage.getItem('notificationGranted');
+        
+        // 알림 권한이 허용되지 않았을 때
         if(!notificationGranted) {
             document.getElementById('permissionSnackbar').style.display = 'block';
-        } else {
+        } else { // 알림 권한이 허욜 됐을 때
             document.getElementById('permissionSnackbar').style.display = 'none';
         }
     });
@@ -59,6 +64,7 @@ function snackBar() {
     });
 }
 
+// 사용자의 위도, 경도 값을 가져오는 함수
 function getPosition() {
     const currentGeoLocation = document.getElementById("camera");
 
@@ -67,6 +73,7 @@ function getPosition() {
             timeout: 10 * 1000
         };
 
+        // 위도, 경도 값을 성공적으로 가져왔을 때
         let geoSuccess = async function (position) {
             // Do magic with location
             const positionObj = {
@@ -74,6 +81,7 @@ function getPosition() {
                 longitude : position.coords.longitude
             }
 
+            // 서버로 위도, 경도 값 전송 (POST)
             await fetch('/location/set', {
                 method: 'POST',
                 headers: {
@@ -104,6 +112,7 @@ function getPosition() {
     };
 }
 
+// 사용자로부터 입력을 받고 쿼리 스트링을 가지고 search.html로 리다이랙션하는 함수
 function performSearch() {
     // Get the value entered in the search input
     const searchInputValue = document.getElementById('searchInput').value;
@@ -114,6 +123,7 @@ function performSearch() {
     }
 }
 
+// 카메라 기능과 사진을 로드하는 기능의 함수
 function loadImage() {
     let camera = document.getElementById('camera');
     let frame = document.getElementById('frame');
@@ -155,6 +165,7 @@ function loadImage() {
 //     }
 // }
 
+// 알림 권한 허용을 요청하고 확인하는 함수
 function checkNotification() {
     document.getElementById('closeSnackbarBtn').addEventListener('click', function() {
         Notification.requestPermission()
@@ -175,6 +186,7 @@ function checkNotification() {
     })
 }
 
+// 주차장 관련 정보를 서버로부터 가져오는 함수 (GET)
 async function getParkingInformation(locationName) {
     const queryString = `/home?name=${locationName}`;
     const urlWithQueryString = `/tempUrl/${queryString}`;
@@ -187,6 +199,7 @@ async function getParkingInformation(locationName) {
     const parkingInformation = response.map((x) => x.data);
 }
 
+// 기록 목록 정보를 서버로부터 가져오는 함수 (GET)
 async function getHistoryInformation(id) {
     const queryString = `/home/history/${id}`;
     const urlWithQueryString = `/tempUrl/${queryString}`;
@@ -217,6 +230,7 @@ document.getElementById('searchInput').addEventListener('keydown', function(even
     }
 });
 
+// main 함수
 function main() {
     splashEffect();
     snackBar();
