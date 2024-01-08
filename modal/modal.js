70d8374c6e62635e87146f03e5215d1f10204ef4
admin <admin@adminui-MacBookPro.local>
2024-01-08 11:11:33 +0900

    🎉[FEAT] : 모달창에 현재 위치 동적 확인 기능 구현

--- modal/modal.js
+++ modal/modal.js
@@ -1,5 +1,6 @@
 // 페이지 로딩 시 모달 열기
 document.addEventListener('DOMContentLoaded', function() {
+  uploadModalContent();
   openModal();
 });
 
@@ -19,4 +20,48 @@ function closeModal() {
 function returnResult(value) {
   closeModal(); // 모달 닫기
   console.log(value); // 여기에서는 콘솔에 결과를 출력하도록 했습니다. 원하는 작업을 수행하면 됩니다.
+}
+
+// 모달 내용 업로드
+function uploadModalContent() {
+  // 현재 위치 가져오기
+  navigator.geolocation.getCurrentPosition(getSuccess, getError);
+
+  // 가져오기 성공
+  async function getSuccess(position) {
+    // 위도
+    const latitude = position.coords.latitude;
+    // 경도
+    const longitude = position.coords.longitude;
+
+    console.log(latitude, longitude);
+
+    let params = new URLSearchParams();
+    params.append('latitude', latitude.toString());
+    params.append('longitude', longitude.toString());
+
+    console.log(params.toString());
+
+    // URL과 쿼리 문자열 합치기
+    let url = '/parking/withLocation' + params.toString();
+
+    // 서버와 GET 통신
+    // const response = await fetch(url);
+    // const jsonData = response.json();
+
+    // const parkingName = jsonData.data.parkingList[0].name;
+    const parkingName = 'Minwoo';
+    updateText(parkingName);
+  }
+
+  // 가지오기 실패(거부)
+  function getError() {
+    alert('Geolocation Error');
+  }
+}
+
+// HTML text update
+function updateText(location) {
+  let locationContent = document.getElementById('location-content');
+  locationContent.innerHTML = `'${location}'에 계신가요?`;
 }
